182580c25f083bd7592b1a3117f10ad36031cdd0
Jauwwad <jauwwad.nallamandu786@gmail.com>
2024-12-07 12:04:31 +0530

    2

---
+++ Bomber.py
@@ -0,0 +1,14 @@
+import pyautogui as py
+import time
+
+message = input("enter a sentence :")
+count = 1 
+n=int(input("Enter number of repeation:"))
+time.sleep(0.1)
+
+for i in range (n):
+    py.typewrite(message + " " + str(count))
+    py.press("Enter")
+    count = count + 1
+
+    
